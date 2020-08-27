00e5f2e5088cb456d5c04ced9dcad9ceedec5a68
Mengty <vongmengty55@gmail.com>
2020-08-27 15:40:16 +0700

    add test error

--- test/mongo.test.js
+++ test/mongo.test.js
@@ -25,6 +25,15 @@ describe('insert', () => {
     const insertedUser = await users.findOne({_id: 'some-user-id'});
     expect(insertedUser).toEqual(mockUser);
   });
+  it('should insert a doc into collection', async () => {
+    const users = db.collection('users');
+   
+    const mockUser = {_id: 'some-user-id', name: 'John'};
+    await users.insertOne(mockUser);
+   
+    const insertedUser = await users.findOne({_id: 'some-user-id'});
+    expect(insertedUser).toEqual(1);
+  });
  
   afterAll(async () => {
     await connection.close();
